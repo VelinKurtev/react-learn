d1b57e6c51623f5ad820feef9e23ad656b95babf
Velin Kurtev <velin.kurtev@dreambox.com>
2024-09-05 12:30:07 +0300

    Import images

--- code/03 React Essentials/01-starting-project/src/App.jsx
+++ code/03 React Essentials/01-starting-project/src/App.jsx
@@ -1,3 +1,5 @@
+import reactImg from './assets/react-core-concepts.png';
+
 const reactDesc = ['Fundamental', 'Crucial', 'Core'];
 
 function getRandomInt(max) {
@@ -7,8 +9,7 @@ function getRandomInt(max) {
 function Header() {
   return (
 		<header>
-      
-			<img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+			<img src={reactImg} alt="Stylized atom" />
 			<h1>React Essentials</h1>
 			<p>
        {reactDesc[getRandomInt(2)]} React concepts you will need for almost any app you are going to build!
