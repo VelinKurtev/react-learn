abe0ac3a21e8603e8f18141360aac65f8b9b4aaf
Velin Kurtev <velin.kurtev@dreambox.com>
2024-09-05 12:23:50 +0300

    Added dynamic code

--- code/03 React Essentials/01-starting-project/src/App.jsx
+++ code/03 React Essentials/01-starting-project/src/App.jsx
@@ -1,11 +1,17 @@
+const reactDesc = ['Fundamental', 'Crucial', 'Core'];
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1))
+}
+
 function Header() {
   return (
 		<header>
+      
 			<img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
 			<h1>React Essentials</h1>
 			<p>
-				Fundamental React concepts you will need for almost any app you are
-				going to build!
+       {reactDesc[getRandomInt(2)]} React concepts you will need for almost any app you are going to build!
 			</p>
 		</header>
 	);
